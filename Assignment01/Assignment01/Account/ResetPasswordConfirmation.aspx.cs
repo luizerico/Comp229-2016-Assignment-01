734d2bb43fd43da5ef2b07b35ff266f2c1c9376c
luizerico <lerico123>
2016-09-26 16:13:34 -0400

    Project creation on VisualStudio.

---
+++ Assignment01/Assignment01/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Assignment01.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Assignment01/Assignment01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignment01.Startup))]
+namespace Assignment01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
